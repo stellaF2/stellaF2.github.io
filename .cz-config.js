ea2c4dc8499c3710736ed5664931ab64ac1744e5
StellaYangFan <1062878468@qq.com>
2020-07-10 19:23:51 +0800

    chore: translate cz tips into Chinese

--- .cz-config.js
+++ .cz-config.js
@@ -1,24 +1,21 @@
 module.exports = {
 
   types: [
-    { value: 'feat', name: 'feat:     A new feature' },
-    { value: 'fix', name: 'fix:      A bug fix' },
-    { value: 'docs', name: 'docs:     Documentation only changes' },
-    { value: 'style', name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)' },
-    { value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature' },
-    { value: 'perf', name: 'perf:     A code change that improves performance' },
-    { value: 'test', name: 'test:     Adding missing tests' },
-    { value: 'build', name: 'test:     Adding missing tests' },
-    { value: 'chore', name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation' },
-    { value: 'revert', name: 'revert:   Revert to a commit' },
-    { value: 'WIP', name: 'WIP:      Work in progress' },
+    { value: 'feat', name: 'feat:     新增特性' },
+    { value: 'fix', name: 'fix:      修复 Bug' },
+    { value: 'docs', name: 'docs:     文档变更' },
+    { value: 'style', name: 'style:    代码格式变更（不影响功能）' },
+    { value: 'refactor', name: 'refactor: 代码重构' },
+    { value: 'perf', name: 'perf:     改善性能' },
+    { value: 'test', name: 'test:     添加测试功能' },
+    { value: 'build', name: 'build:    变更项目构建或外部依赖' },
+    { value: 'chore', name: 'chore:    变更构建流程或辅助工具' },
+    { value: 'revert', name: 'revert:   回退' },
   ],
 
   scopes: [
-    { name: 'accounts' },
-    { name: 'admin' },
-    { name: 'exampleScope' },
-    { name: 'changeMe' },
+    { name: 'component' },
+    { name: 'config' },
   ],
 
   // it needs to match the value for field type. Eg.: 'fix'
@@ -34,15 +31,15 @@ module.exports = {
     */
   // override the messages, defaults are as follows
   messages: {
-    type: 'Select the type of change that you\'re committing:',
-    scope: '\nDenote the SCOPE of this change (optional):',
+    type: '选择即将提交的修改类型:',
+    scope: '本次变更作用域（可选）:',
     // used if allowCustomScopes is true
-    customScope: 'Denote the SCOPE of this change:',
-    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
-    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-    breaking: 'List any BREAKING CHANGES (optional):\n',
-    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
-    confirmCommit: 'Are you sure you want to proceed with the commit above?',
+    customScope: '自定义变更作用域:',
+    subject: '简单描述本次变更，动词开头:\n',
+    body: '详细描述变更（可选），使用 "|" 另起一行:\n',
+    breaking: '列出重大变更（可选）:\n',
+    footer: '列出本次变更关闭的 ISSUES （可选），如： #31, #34:\n',
+    confirmCommit: '确定提交上述信息？',
   },
 
   allowCustomScopes: true,
